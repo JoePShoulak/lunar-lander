770eb8780fa48fa3f4941677d3e587cccc40cb9d
Joe P Shoulak <joe@squarecompass.io>
2023-03-25 22:45:11 -0500

    found the hover throttle amount

--- libraries/Rocket.js
+++ libraries/Rocket.js
@@ -6,8 +6,8 @@ class Rocket {
   static rotScale = 0.00003;
 
   static throttleRate = 0.01;
-  static throttleMin = 0.1;
-  static throttleMax = 0.6;
+  static throttleMin = 0.101125; // Just for now so it's easy to get to hover
+  static throttleMax = 0.601125; // (these normally should have nothing after 0.X)
 
   constructor(x, y, size) {
     this.body = Bodies.rectangle(x, y, size, size, {
@@ -16,7 +16,7 @@ class Rocket {
     });
 
     this.engineOn = false;
-    this.throttle = 0.1;
+    this.throttle = 0.101125; // starting here to be able to get to hover
   }
 
   get thrustVec() {
@@ -34,6 +34,7 @@ class Rocket {
     );
   }
 
+  // Rocket currently seems to hover best at throttle=0.331125
   setThrottle(val) {
     val = Math.min(val, Rocket.throttleMax);
     val = Math.max(val, Rocket.throttleMin);
